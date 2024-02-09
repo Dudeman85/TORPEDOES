2507837bf2a6ad928eeee8532f2d8a902fa2f90e
viksteri kappi <viksterikappi@kamk.fi>
2024-02-09 14:30:12 +0200

    torpedo icon edits

--- sandboxes/TORPEDOES/main.cpp
+++ sandboxes/TORPEDOES/main.cpp
@@ -233,7 +233,7 @@ int main()
 
 	Animation countdownAnim = AnimationsFromSpritesheet("UI_Countdown_Ver2.png", 5, 1, vector<int>(5, 1000))[0];
 	ecs::Entity countdown = ecs::NewEntity();
-	ecs::AddComponent(countdown, Transform{ .position = Vector3(1475, -1200, 10), .scale = Vector3(60, 100, 0) });
+	ecs::AddComponent(countdown, Transform{ .position = Vector3(1475, -1270, 10), .scale = Vector3(60, 100, 0) });
 	ecs::AddComponent(countdown, SpriteRenderer{});
 	ecs::AddComponent(countdown, Animator{});
 	AnimationSystem::AddAnimation(countdown, countdownAnim, "CountDown");
