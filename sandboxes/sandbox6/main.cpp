00a135b99714db07dd07bcac29ebaf9431816b6f
Sauli Hanell <saulihanell@kamk.fi>
2024-02-09 10:17:51 +0200

    sandbox6

--- sandboxes/sandbox6/main.cpp
+++ sandboxes/sandbox6/main.cpp
@@ -7,22 +7,6 @@
 
 #include "engine/Input.h"
 
-int main() 
-{
-    // Set the window dimensions and title
-    int width = 800;
-    int height = 600;
-    const char* title = "GLFW Key Callback Example";
-
-    // Initialize GLFW and create a window with a key callback
-    GLFWwindow* window = initializeGLFW(width, height, title);
-    if (!window) {
-        return -1;
-    }
-
-
-    return key;
-}
 
 void OnSpacePressed(int a)
 {
