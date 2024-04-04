f23eb9eea48702b6c7b24d9632edd0a4f53e1a1a
Aleksi Anderson <aleksianderson@kamk.fi>
2024-04-04 13:21:02 +0300

    Finished placement editor

--- sandboxes/TORPEDOES/main.cpp
+++ sandboxes/TORPEDOES/main.cpp
@@ -175,47 +175,53 @@ int main()
 			glfwSetWindowShouldClose(window, true);
 
 		//Object editor
+		const float editorSpeed = 0.5;
 		//Hold numpad 0 to toggle rotate
 		if (glfwGetKey(window, GLFW_KEY_KP_0))
 		{
 			//Numpad 8456 move
 			if (glfwGetKey(window, GLFW_KEY_KP_4))
-				TransformSystem::Rotate(placementEditor, Vector3(0, 0, 5));
+				TransformSystem::Rotate(placementEditor, Vector3(0, 0, 5) * editorSpeed);
 			if (glfwGetKey(window, GLFW_KEY_KP_6))
-				TransformSystem::Rotate(placementEditor, Vector3(0, 0, -5));
+				TransformSystem::Rotate(placementEditor, Vector3(0, 0, -5) * editorSpeed);
 			if (glfwGetKey(window, GLFW_KEY_KP_8))
-				TransformSystem::Rotate(placementEditor, Vector3(-5, 0, 0));
+				TransformSystem::Rotate(placementEditor, Vector3(-5, 0, 0) * editorSpeed);
 			if (glfwGetKey(window, GLFW_KEY_KP_5))
-				TransformSystem::Rotate(placementEditor, Vector3(5, 0, 0));
+				TransformSystem::Rotate(placementEditor, Vector3(5, 0, 0) * editorSpeed);
 			if (glfwGetKey(window, GLFW_KEY_KP_7))
-				TransformSystem::Rotate(placementEditor, Vector3(0, 5, 0));
+				TransformSystem::Rotate(placementEditor, Vector3(0, 5, 0) * editorSpeed);
 			if (glfwGetKey(window, GLFW_KEY_KP_9))
-				TransformSystem::Rotate(placementEditor, Vector3(0, -5, 0));
+				TransformSystem::Rotate(placementEditor, Vector3(0, -5, 0) * editorSpeed);
 		}
 		else
 		{
 			//Numpad 8456 move
 			if (glfwGetKey(window, GLFW_KEY_KP_8))
-				TransformSystem::Translate(placementEditor, Vector3(0, 10, 0));
+				TransformSystem::Translate(placementEditor, Vector3(0, 10, 0) * editorSpeed);
 			if (glfwGetKey(window, GLFW_KEY_KP_4))
-				TransformSystem::Translate(placementEditor, Vector3(-10, 0, 0));
+				TransformSystem::Translate(placementEditor, Vector3(-10, 0, 0) * editorSpeed);
 			if (glfwGetKey(window, GLFW_KEY_KP_5))
-				TransformSystem::Translate(placementEditor, Vector3(0, -10, 0));
+				TransformSystem::Translate(placementEditor, Vector3(0, -10, 0) * editorSpeed);
 			if (glfwGetKey(window, GLFW_KEY_KP_6))
-				TransformSystem::Translate(placementEditor, Vector3(10, 0, 0));
+				TransformSystem::Translate(placementEditor, Vector3(10, 0, 0) * editorSpeed);
 		}
 		//+- scale
 		if (glfwGetKey(window, GLFW_KEY_KP_ADD))
-			TransformSystem::Scale(placementEditor, 1);
+			TransformSystem::Scale(placementEditor, 1 * editorSpeed);
 		if (glfwGetKey(window, GLFW_KEY_KP_SUBTRACT))
-			TransformSystem::Scale(placementEditor, -1);
+			TransformSystem::Scale(placementEditor, -1 * editorSpeed);
 		//Print spawn Function
 		if (glfwGetKey(window, GLFW_KEY_KP_ENTER))
 		{
 			auto& et = ecs::GetComponent<Transform>(placementEditor);
-			std::cout << et.position.ToString() << ", " << et.rotation.ToString() << ", " << et.rotation.ToString() << std::endl;
+			std::cout << et.position.ToString() << ", " << et.rotation.ToString() << ", " << et.scale.ToString() << std::endl;
 		}
-
+		//Reset Transforms
+		if (glfwGetKey(window, GLFW_KEY_KP_DECIMAL))
+		{
+			TransformSystem::SetRotation(placementEditor, 0);
+			TransformSystem::SetScale(placementEditor, 20);
+		}
 
 		input::update();
 
