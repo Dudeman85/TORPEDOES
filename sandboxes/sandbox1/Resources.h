66de3e6b8b9b7923bdef2383a36fb7fbbdf2edeb
viksteri kappi <viksterikappi@kamk.fi>
2024-04-11 15:56:22 +0300

    sandbox update, level update

--- sandboxes/sandbox1/Resources.h
+++ sandboxes/sandbox1/Resources.h
@@ -4,49 +4,56 @@
 
 namespace resources
 {
-	std::unordered_map<std::string, engine::Texture*> uiTextures;
-	std::unordered_map<std::string, engine::Texture*> menuTextures;
+	using namespace engine;
 
-	std::unordered_map<std::string, engine::Model*> models;
+	std::unordered_map<std::string, Texture*> uiTextures;
+	std::unordered_map<std::string, Texture*> menuTextures;
+	std::unordered_map<std::string, Texture*> modelTextures;
 
-	engine::Font* niagaraFont;
+	std::unordered_map<std::string, Model*> models;
+
+	Font* niagaraFont;
 
 	Tilemap* level1Map;
 
-	engine::Animation explosionAnimation;
-	engine::Animation crowdAnims;
-	engine::Animation countdownAnim;
+	Animation explosionAnimation;
+	Animation crowdAnims;
+	Animation countdownAnim;
+	Animation divingAnim;
+
 
 	//Load all the global resources here
 	//Stuff that is only used in one system can be loaded there 
-	void LoadResources(engine::Camera* cam)
+	void LoadResources(Camera* cam)
 	{
 		uiTextures = engine::PreloadTextures("GUI");
 		menuTextures = engine::PreloadTextures("menuUI", true, GL_LINEAR_MIPMAP_NEAREST);
+		modelTextures = engine::PreloadTextures("3dmodels");
 
 		models = engine::PreloadModels("3dmodels");
 
-		niagaraFont = new engine::Font("Niagara Solid.ttf", 0, 0, 48);
+		niagaraFont = new Font("Niagara Solid.ttf", 0, 0, 48);
 
 		level1Map = new Tilemap(cam);
-		level1Map->loadMap("/levels/level1.tmx");
+		level1Map->loadMap("/levels/level3.tmx");
 
-		explosionAnimation = engine::AnimationsFromSpritesheet("/spritesheets/explosion.png", 6, 1, std::vector<int>(6, 150))[0];
-		crowdAnims = engine::AnimationsFromSpritesheet("/spritesheets/CrowdCheer14.png", 3, 1, std::vector<int>(3, 150))[0];
-		countdownAnim = engine::AnimationsFromSpritesheet("/spritesheets/UI_Countdown_Ver2.png", 5, 1, std::vector<int>(5, 1000))[0];
+		explosionAnimation = AnimationsFromSpritesheet("/spritesheets/explosion.png", 6, 1, vector<int>(6, 150))[0];
+		crowdAnims = AnimationsFromSpritesheet("/spritesheets/CrowdCheer14.png", 3, 1, vector<int>(3, 150))[0];
+		countdownAnim = AnimationsFromSpritesheet("/spritesheets/UI_Countdown_Ver2.png", 5, 1, vector<int>(5, 1000))[0];
+		divingAnim = AnimationsFromSpritesheet("/spritesheets/Diving_Sheet.png", 4, 1, vector<int>(4, 100))[0];
 	}
 
 	void UnloadResources()
 	{
-		for (const auto& model : models)
+		for (const auto model : models)
 		{
 			delete model.second;
 		}
-		for (const auto& texture : uiTextures)
+		for (const auto texture : uiTextures)
 		{
 			delete texture.second;
 		}
-		for (const auto& texture : menuTextures)
+		for (const auto texture : menuTextures)
 		{
 			delete texture.second;
 		}
